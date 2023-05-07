b0155debf2fab4580a70e99aec3b309ccec54154
shunakid <shunakid@icloud.com>
2023-05-07 15:09:27 +0900

    feat: firebaseの設定

---
+++ src/services/firebase/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDvDOd8mibDZaqJfMcSyiqHxPiuDHwuz-k',
+  authDomain: 'glampt-b851d.firebaseapp.com',
+  projectId: 'glampt-b851d',
+  storageBucket: 'glampt-b851d.appspot.com',
+  messagingSenderId: '646590061390',
+  appId: '1:646590061390:web:0f2bf9c52e575ced8bdc1f',
+}
+
+const app = initializeApp(firebaseConfig)
+export default app
